9ca72a793fb661dcb50d524794b57676482206a9
Jen3199 <jenlaw32@hotmail.com>
2024-09-23 09:59:13 -0400

    Add files via upload

---
+++ function_demo.py
@@ -0,0 +1,8 @@
+# This program demonstrates a function.
+# First, we define a function name message.
+def message():
+        print('I am Jennifer,')
+        print('King of the britons.')
+
+# Call the message function.
+message()
